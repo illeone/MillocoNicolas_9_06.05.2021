92d78cfd862a1c312dd10947d29fe5ba14f34bc2
Nicolas <nicolasmilloco@hotmail.com>
2024-01-04 18:33:28 +0100

    Corrections des sélecteurs de formulaire pour le login de admin

--- Billed-app-FR-Front-main/src/containers/Login.js
+++ Billed-app-FR-Front-main/src/containers/Login.js
@@ -16,6 +16,7 @@ export default class Login {
     formAdmin.addEventListener("submit", this.handleSubmitAdmin)
   }
   handleSubmitEmployee = e => {
+    console.log("handleSubmitEmployee")
     e.preventDefault()
     const user = {
       type: "Employee",
@@ -41,8 +42,8 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
