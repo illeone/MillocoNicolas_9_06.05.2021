90aa6f1c90d31b83bc61dab4f02c4ef2fb6e7a92
Nicolas <nicolasmilloco@hotmail.com>
2024-01-12 18:17:29 +0100

    Ajout du test d'intégration pour la gestion de l'erreur 500

--- Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -160,5 +160,20 @@ describe("Given I am a user connected as Employee", () => {
         const message =  screen.getByText(/Erreur 404/)
         expect(message).toBeTruthy()
     })
+
+    test("fetches messages from an API and fails with 500 message error", async () => {
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+        
+      window.onNavigate(ROUTES_PATH.Bills)
+      document.body.innerHTML = BillsUI({error: "Erreur 500"});
+      const message = screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy()
+    })
   })
 });
