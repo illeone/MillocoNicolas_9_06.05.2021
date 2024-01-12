00aabc0e4d48fea8f72e4bbc3c9a9966522784b6
Nicolas <nicolasmilloco@hotmail.com>
2024-01-12 19:15:59 +0100

    Ajout du test unitaire pour l'icône mail active dans NewBill

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -1,18 +1,28 @@
 /**
  * @jest-environment jsdom
  */
-
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES_PATH } from "../constants/routes"
+import router from "../app/Router"
 import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then icon mail in vertical layout should be highlighted", () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      let root = document.createElement('div');
+      root.setAttribute('id','root');
+      window.location.assign(ROUTES_PATH.NewBill);
+      document.body.appendChild(root);
+      router();
+      const mailIcon = screen.getByTestId('icon-mail');
+      expect(mailIcon.classList.contains('active-icon')).toBeTruthy();
+
     })
-  })
+  });
 })
